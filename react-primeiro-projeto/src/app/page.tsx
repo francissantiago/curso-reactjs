737f30f37817cbc46b15419222fb8063feb41380
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-01-26 05:26:26 -0300

    Usando vários componentes

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,32 +1,14 @@
-/* Importação de componente do tipo default
-import Square from '@/components/Geo';
-*/
+/* Importação de componentes */
 
-/*
-* Importação de componentes padrões e não padrões
-*/
-import Square, {Circle } from '@/components/Geo';
+import { GeoForm } from "@/components/GeoForms";
 
-/* Posso escrever assim
-function Page() {
-  return (
-    <div>
-      <h1 className="font-bold text-2xl">Olá Mundo</h1>
-      <h3>Algum outro texto</h3>
-    </div>
-  );
-}
-*/
-
-/* Ou assim: */
 const Page = () => {
   return (
     <div>
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Algum outro texto</h3>
 
-      <Square />
-      <Circle />
+      <GeoForm />
     </div>
   );
 }
