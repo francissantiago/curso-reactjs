501a6201dbf1be8923f1b95619371f4aef3b165e
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-01-30 17:00:23 -0300

    Passando props para um componente

--- react-primeiro-projeto/src/components/Person.tsx
+++ react-primeiro-projeto/src/components/Person.tsx
@@ -1,31 +1,26 @@
-const getWeekday = (today: Date) => {
-    return new Intl.DateTimeFormat('pt-BR', { weekday: 'long' }).format(today);
+type Props = {
+    name: string;
+    avatar: string;
+    roles: string[];
 }
 
-export const Person = () => {
+export const Person = ({ name, avatar, roles}: Props) => {
     /* Regra 1: Retorno de um único elemento padrão(apenas um return) */
     /* Regra 2: Feachar todas as tags */
     /* Regra 3: camelCase em todas as propriedades que tenham 2 palavras como className, onClick, etc. */
 
-    const data = {
-        name: 'Elon Musk',
-        avatar: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Elon_Musk_Colorado_2022_%28cropped2%29.jpg',
-        roles: ['CEO da Tesla', 'CEO da SpaceX']
-    }
-
-    const today: Date = new Date();
     return (
-        <>
-            <h1>{data.name} - {getWeekday(today)}</h1>
+        <div className="p-3">
+            <h1>{name}</h1>
             <img
-                src={data.avatar}
-                alt={data.name}
+                src={avatar}
+                alt={name}
                 className="w-40"
             /> 
             <ul>
-                <li>{data.roles[0]}</li>
-                <li>{data.roles[1]}</li>
+                <li>{roles[0]}</li>
+                <li>{roles[1]}</li>
             </ul>
-        </>
+        </div>
     );
 }
