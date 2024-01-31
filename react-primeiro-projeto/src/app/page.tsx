7faefd1915242c5ad2b21157a9f520b6e879c48d
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-01-31 05:44:40 -0300

    Renderização Condicional

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,7 +1,5 @@
 /* Importação de componentes */
 import { Card } from "@/components/Card";
-import { Circle } from "@/components/Circle";
-import { Person } from "@/components/Person";
 
 const Page = () => {
   return (
@@ -9,13 +7,7 @@ const Page = () => {
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Algum outro texto</h3>
 
-      <Card>
-        <>
-          <h3 className="text-3xl font-bold italic">"Frase de efeito</h3>
-          <p className="text-right text-sm">- Autor desconhecido</p>
-          <Circle />
-        </>
-      </Card>
+      <Card phrase="Alguma frase TOP" author="Algum cara" />
     </div>
   );
 }
--- react-primeiro-projeto/src/components/Card.tsx
+++ react-primeiro-projeto/src/components/Card.tsx
@@ -1,13 +1,13 @@
-import { ReactNode } from "react";
-
 type Props = {
-    children: ReactNode;
+    phrase: string;
+    author?: string;
 }
 
-export const Card = ({ children }: Props) => {
+export const Card = ({ phrase, author }: Props) => {
     return (
         <div className="w-96 border-2 border-red-600 p-3 text-3xl text-center italic">
-            {children}
+            <h3 className="text-3xl font-bold italic">"{phrase}"</h3>
+            <p className="text-right text-sm">- {author ?? 'Autor Desconhecido'}</p>
         </div>
     );
 }
