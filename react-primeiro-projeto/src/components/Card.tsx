583f084c5d6bfd36728de02e8a70b2fd806b8ea8
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-02-01 05:29:41 -0300

    [M2][#10]Operador lógico &&

--- react-primeiro-projeto/src/components/Card.tsx
+++ react-primeiro-projeto/src/components/Card.tsx
@@ -7,7 +7,12 @@ export const Card = ({ phrase, author }: Props) => {
     return (
         <div className="w-96 border-2 border-red-600 p-3 text-3xl text-center italic">
             <h3 className="text-3xl font-bold italic">"{phrase}"</h3>
-            <p className="text-right text-sm">- {author ?? 'Autor Desconhecido'}</p>
+            {author &&
+                <p className="text-right text-sm">- {author}</p>
+            }
+            {!author &&
+                <p className="text-right text-sm italic">- Autor Desconhecido</p>    
+            }
         </div>
     );
 }
