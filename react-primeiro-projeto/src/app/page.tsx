98b7951eb9dff14ed14c19e486173ff80ad6da57
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-02-02 05:39:29 -0300

    [M2][#11]Renderizando Listas

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,13 +1,22 @@
 /* Importação de componentes */
-import { Card } from "@/components/Card";
+import { peopleList } from "@/data/peopleList";
 
 const Page = () => {
+
+
+
   return (
     <div>
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Algum outro texto</h3>
 
-      <Card phrase="Alguma frase TOP" author="Algum cara" />
+      {peopleList.length > 0 &&
+        <ul>
+          {peopleList.map(person =>
+            <li key={person.id}>{person.name} - {person.profession}</li>
+          )}
+        </ul>
+      }
     </div>
   );
 }
