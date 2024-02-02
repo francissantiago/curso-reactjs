6d554e818889f36d47fcca8de00703c4164b4cd0
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-02-02 17:16:42 -0300

    Greeting condicional 2

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -8,21 +8,16 @@ const Page = () => {
   }).format();
 
   const hour = new Date().getHours();
-  let greeting = '';
-
-  if(hour > 0 && hour < 12) {
-    greeting = 'Bom dia!';
-  } else if(hour >= 12 && hour < 18) {
-    greeting = 'Boa tarde!';
-  } else if(hour >= 18 && hour <= 23){
-    greeting = 'Boa noite!';
-  }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-white
     bg-gradient-to-r from-sky-500 to-indigo-500">
       <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl font-bold">{greeting}</div>
+      <div className="text-5xl font-bold">
+        {hour >= 0 && hour < 12 && "Bom dia!"}
+        {hour >= 12 && hour < 18 && "Boa tarde!"}
+        {hour >= 18 && hour <= 23 && "Boa noite!"}
+      </div>
     </div>
   );
 }
