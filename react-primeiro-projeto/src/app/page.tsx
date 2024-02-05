b5e6aaa2576adf0f0909b64cfd2658e791fd1e9b
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-02-05 05:34:23 -0300

    [M2][#15]Rating via emoji (fácil)

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,4 +1,5 @@
 /* Importação de componentes */
+import { EmojiRating } from "@/components/EmojiRating";
 
 const Page = () => {
 
@@ -10,14 +11,8 @@ const Page = () => {
   const hour = new Date().getHours();
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white
-    bg-gradient-to-r from-sky-500 to-indigo-500">
-      <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl font-bold">
-        {hour >= 0 && hour < 12 && "Bom dia!"}
-        {hour >= 12 && hour < 18 && "Boa tarde!"}
-        {hour >= 18 && hour <= 23 && "Boa noite!"}
-      </div>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <EmojiRating rate={3.2} />
     </div>
   );
 }
---
+++ react-primeiro-projeto/src/components/EmojiRating.tsx
@@ -0,0 +1,19 @@
+type Props = {
+    rate: number;
+}
+
+export const EmojiRating = ({ rate }: Props) => {
+    if(rate > 5) rate = 5;
+    if(rate < 0) rate = 0;
+
+    const rateInt = Math.floor(rate);
+
+    const stars = '😁'.repeat(rateInt) + '😶'.repeat(5 - rateInt);
+
+    return (
+        <div className="flex item-center text-6xl">
+            <div className="bg-gray-700 p-2 rounded">{rate.toFixed(1)}</div>
+            <div className="ml-3">{stars}</div>
+        </div>
+    );
+}
