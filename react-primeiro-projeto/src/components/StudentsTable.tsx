758e405d83b266719e869f41a2c38e8e7b107e1c
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-02-07 20:36:09 -0300

    [M2][#18] Tabela de notas estudantis 2

--- react-primeiro-projeto/src/components/StudentsTable.tsx
+++ react-primeiro-projeto/src/components/StudentsTable.tsx
@@ -16,9 +16,24 @@ export const StudentsTable = ({ students }: Props) => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    
-                </tr>
+                {students.map(item => (
+                    <tr key={item.id} className="text-gray-800 bg-gray-400 border-b border-gray-600">
+                        <td className="p-3 flex items-center">
+                            <img src={item.avatar} alt={item.name} className="w-10 h-10 rounded-full mr-3"/>
+                            <div>
+                                <div className="font-bold">{item.name}</div>
+                                <div>{item.email}</div>
+                            </div>
+                        </td>
+                        <td className="p-3">
+                            {item.active && <div className="px-2 py-1 inline-block rounded-md border border-green-800 bg-green-600 text-white text-xs">Active</div>}
+                            {!item.active && <div className="px-2 py-1 inline-block rounded-md border border-red-800 bg-red-600 text-white text-xs">Inactive</div>}
+                        </td>
+                        <td className="p-3">{item.grade1.toFixed(1)}</td>
+                        <td className="p-3">{item.grade2.toFixed(1)}</td>
+                        <td className="p-3 font-bold">{item.active ? ((item.grade1 + item.grade2) / 2).toFixed(1) : '--'}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
