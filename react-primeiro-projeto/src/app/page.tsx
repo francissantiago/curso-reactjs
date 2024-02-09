c6c22c8cc6c32f1f58346bd17e3215900de0ef1d
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-02-09 19:37:23 -0300

    [M3][#02] Forma errada de passar eventos

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -2,12 +2,25 @@
 
 const Page = () => {
 
-  const handleButtonClick = () => {
-    alert("Funcionou");
+  // Não fazer assim!
+  const handleClick = () => {
+    alert("Tudo certo!");
   }
+
+  const avisar = (msg: string) => {
+    alert(msg);
+  }
+
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <button onClick={handleButtonClick} className="p-3 bg-blue-700 text-white rounded-md">Clique aqui</button>
+     {/*
+      Não fazer assim!
+      <button onClick={handleClick()} className="p-3 bg-blue-700 text-white rounded-md mr-3">Clique aqui</button>
+    */}
+
+      <button onClick={() => avisar('mensagem 1')} className="p-3 bg-blue-700 text-white rounded-md mr-3">Clique aqui</button>
+      <button onClick={() => avisar('mensagem 2')} className="p-3 bg-blue-700 text-white rounded-md mr-3">Clique aqui</button>
+
 
     </div>
   );
