6bb61efb990f852364a2fc98fa1ad886364ff0af
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-02-09 19:21:16 -0300

    Preparação para Módulo 3

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,12 +1,7 @@
-/* Importação de componentes */
-import { StudentsTable } from "@/components/StudentsTable";
-import { students } from "@/data/students";
-
 const Page = () => {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-5xl mb-5">Lista de estudantes</h1>
-      <StudentsTable students={students} />
+    <div>
+      
     </div>
   );
 }
--- react-primeiro-projeto/src/types/Student.ts
+++
@@ -1,9 +0,0 @@
-export type Student = {
-    id: number;
-    active: boolean;
-    name: string;
-    email: string;
-    avatar: string;
-    grade1: number;
-    grade2: number;
-}
