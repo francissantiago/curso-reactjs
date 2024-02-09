2df9341967c2f42b6fb1d6419313687ff068d7ca
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-02-09 19:22:14 -0300

    [M3][#01] Adicionando evento de clique

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -1,8 +1,16 @@
+"use client"
+
 const Page = () => {
+
+  const handleButtonClick = () => {
+    alert("Funcionou");
+  }
   return (
-    <div>
-      
+    <div className="w-screen h-screen flex justify-center items-center">
+      <button onClick={handleButtonClick} className="p-3 bg-blue-700 text-white rounded-md">Clique aqui</button>
+
     </div>
   );
 }
+
 export default Page;
