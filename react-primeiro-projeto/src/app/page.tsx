47d2f496027f76e49d089f1d8a2b00f77bfcad1d
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-02-14 20:11:22 -0300

    [M3][#05] O que é um State?

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -3,18 +3,16 @@
 import { FormEvent } from "react";
 
 const Page = () => {
-
-  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    alert("Opa, mandando...");
+  let count = 0;
+  const handleClickButton = () => {
+    count = count + 1;
+    console.log(count);
   }
+
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <h1 className="text-5xl mb-3">Form de Login</h1>
-      <form onSubmit={handleFormSubmit}>
-        <input type="text" />
-        <input type="submit" value="Enviar" />
-      </form>
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
+      <p>{count}</p>
+      <button onClick={handleClickButton} className="bg-blue-500 p-3">+1</button>
     </div>
   );
 }
