7967b3fd2988d671f86a99cb5fc241afa2812731
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-02-18 20:12:02 -0300

    [M3][#09] States mudando no tempo

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -3,23 +3,23 @@
 import { useState } from "react";
 
 const Page = () => {
-  const [nameInput, setNameInput] = useState('');
+  const [count, setCount] = useState(0);
 
+  /* O state só é alterado(renderizado) no cliente quando a função estiver terminado por completo */
   const handleBtnClick = () => {
-    alert(nameInput);
+    console.log('count 0: ', count);
+    setCount(count + 2);
+    console.log('count 1: ', count);
+    setCount(count + 2);
+    console.log('count 2: ', count);
+    setCount(count + 2);
+    console.log('count 3: ', count);
   }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
-      <input
-        type="text"
-        className="border border-black p-3 text-xl text-black rounded"
-        placeholder="Digite seu nome"
-        value={nameInput}
-        onChange={e => setNameInput(e.target.value)}
-      />
-      <p>Seu nome é: {nameInput}</p>
-      <button onClick={handleBtnClick}>Mostrar valor do campo</button>
+      <p>{count}</p>
+      <button onClick={handleBtnClick} className="bg-blue-700 text-white p-3 rounded-md">+2</button>
     </div>
   );
 }
