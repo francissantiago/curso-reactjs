8d052002f3408517b1392aa6679a013d4ff68744
LightBurdenOfficial <32315718+LightBurdenOfficial@users.noreply.github.com>
2024-02-25 12:20:34 -0300

    [M3][#16] Arrays em States: Usando ID

--- react-primeiro-projeto/src/app/page.tsx
+++ react-primeiro-projeto/src/app/page.tsx
@@ -6,34 +6,31 @@ import { useState } from "react";
 const Page = () => {
   const [itemInput, setItemInput] = useState('');
   const [list, setList] = useState<TodoItem[]>([
-    { label: 'Fazer dever de casa', checked: false},
-    { label: 'Comprar o bolo', checked: false},
+    { id: 1, label: 'Fazer dever de casa', checked: false},
+    { id: 2, label: 'Comprar o bolo', checked: false},
   ]);
 
   const handleAddButton = () => {
     if(itemInput.trim() === '') return;
 
-    setList([...list, {label: itemInput, checked: true}]);
+    setList([...list, {id: list.length + 1, label: itemInput, checked: false}]);
     setItemInput('');
   }
 
-  const deleteItem = (index: number) => {
+  const deleteItem = (id: number) => {
     setList(
-      list.filter((item, key) => key !== index)
+      list.filter(item => item.id !== id)
     );
   }
 
-  const toggleItem = (index: number) => {
+  const toggleItem = (id: number) => {
     let newList = [...list];
-    newList[index].checked = !newList[index].checked;
 
-    /* Utilizar assim quando for buscar ID
     for(let i in newList) {
-      if(index === parseInt(i)){
+      if(newList[i].id === id){
         newList[i].checked = !newList[i].checked;
       }
     }
-    */
 
     setList(newList);
   }
@@ -56,10 +53,10 @@ const Page = () => {
       <p className="my-4">{list.length} itens na lista</p>
 
       <ul className="w-full max-w-lg list-disc pl-5">
-        {list.map((item, index) => (
-          <li key={index}>
-            <input onClick={() => toggleItem(index)} type="checkbox" checked={item.checked} className="w-6 h-6 mr-3" />
-            {item.label} - <button onClick={() => deleteItem(index)} className="hover:underline">[ deletar ]</button>
+        {list.map(item => (
+          <li key={item.id}>
+            <input onClick={() => toggleItem(item.id)} type="checkbox" checked={item.checked} className="w-6 h-6 mr-3" />
+            {item.label} - <button onClick={() => deleteItem(item.id)} className="hover:underline">[ deletar ]</button>
           </li>
         ))}
       </ul>
